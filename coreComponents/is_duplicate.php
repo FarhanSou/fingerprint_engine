9c804bc3889d3253576a5e9ba6e16833938dcbc9
Farhan Bajwa <61959722+FarhanSou@users.noreply.github.com>
2023-02-07 21:05:18 -0800

    Update is_duplicate.php

--- coreComponents/is_duplicate.php
+++ coreComponents/is_duplicate.php
@@ -16,14 +16,36 @@ if(!empty($_POST["data"])) {
     // each enrolled hand has two fingers (index and middle finger)
     // in reality this could be any finger, we just expect two fingers
     // for each hand
-    $enrolled_hands_list = $user_data->enrolled_hands_list;
-    $is_duplicate = check_duplicate($pre_enrolled_finger_data, $enrolled_hands_list);
-    if ($is_duplicate){
-        echo json_encode(true);
+    $enrolled_hands_lists = $user_data->enrolled_hands_list;
+
+
+    foreach ($enrolled_hands_lists as $enrolled_hands_list1) {
+        $enrolled_hands_list = array($enrolled_hands_list1);
+        // print_r($enrolled_hand_array[0]->id);
+        // die();
+        $is_duplicate = check_duplicate($pre_enrolled_finger_data, $enrolled_hands_list);
+        // print_r($response);
+        // die();
+        if($response == 1){
+            // return true;
+            echo getUserDetails($enrolled_hand_array[0]->id);
+            break;
+        }
+        
     }
-    else{
-        echo json_encode(false);
-    }
+
+    // print_r($enrolled_hands_list);
+    // die();
+
+
+
+    // $is_duplicate = check_duplicate($pre_enrolled_finger_data, $enrolled_hands_list);
+    // if ($is_duplicate){
+    //     echo json_encode(true);
+    // }
+    // else{
+    //     echo json_encode(false);
+    // }
 }
 else {
     echo json_encode("error! no data provided in post request");
