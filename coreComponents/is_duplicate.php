c69483e730b698ea1453bc0b5883d8c72564ce07
Farhan Bajwa <61959722+FarhanSou@users.noreply.github.com>
2023-02-07 21:27:59 -0800

    Update is_duplicate.php

--- coreComponents/is_duplicate.php
+++ coreComponents/is_duplicate.php
@@ -19,17 +19,21 @@ if(!empty($_POST["data"])) {
     $enrolled_hands_lists = $user_data->enrolled_hands_list;
 
 
+    $enrolled_hands_lists = $user_data->enrolled_hands_list;
+    print_r($enrolled_hands_lists);
+    die();
+
     foreach ($enrolled_hands_lists as $enrolled_hands_list1) {
         $enrolled_hands_list = array($enrolled_hands_list1);
         // print_r($enrolled_hand_array[0]->id);
         // die();
         $is_duplicate = check_duplicate($pre_enrolled_finger_data, $enrolled_hands_list);
-        // print_r($response);
-        // die();
-        echo $is_duplicate;
+        // echo $is_duplicate;
         // die();
         if($is_duplicate == 1){
             echo $enrolled_hand_array[0]->id;
+            echo 'sd';
+            die();
             // echo json_encode(true);
             // echo getUserDetails($enrolled_hand_array[0]->id);
             break;
